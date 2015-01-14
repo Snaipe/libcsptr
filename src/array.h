aae241068684178840acf68dd30b8f0a4a8e1cbf
Snaipe <franklinmathieu@gmail.com>
2015-01-14 14:44:01 +0100

    Forgot to include header

--- src/array.h
+++ src/array.h
@@ -1,5 +1,29 @@
-#ifndef ARRAY_H_
-# define ARRAY_H_
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2015 Franklin "Snaipe" Mathieu <http://snai.pe/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef CSPTR_ARRAY_H_
+# define CSPTR_ARRAY_H_
 
 # include "smart_ptr.h"
 
@@ -26,4 +50,4 @@ inline void *array_user_meta(void *ptr) {
     return meta ? meta + 1 : NULL;
 }
 
-#endif /* !ARRAY_H_ */
+#endif /* !CSPTR_ARRAY_H_ */

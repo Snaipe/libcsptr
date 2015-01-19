ae7f183510e1df801b3694503c3369da9531884f
Snaipe <franklinmathieu@gmail.com>
2015-01-19 21:30:46 +0100

    Added zero-size test, fixed failling-allocation test.

--- check/test/misc.c
+++ check/test/misc.c
@@ -8,6 +8,11 @@ START_TEST (test_more_args) {
     assert_valid_ptr(ptr);
 } END_TEST
 
+START_TEST (test_zero_size) {
+    void *ptr = smalloc(0, 0, UNIQUE);
+    ck_assert_msg(ptr == NULL, "Expected NULL pointer to be returned.");
+} END_TEST
+
 #ifndef FIXED_SMALLOC_ALLOCATOR
 START_TEST (test_alloc_failure) {
     smalloc_allocator = (s_allocator) {
@@ -15,12 +20,14 @@ START_TEST (test_alloc_failure) {
         lambda(void, (void *ptr) {})
     };
     smart void *ptr = unique_ptr(int);
-    assert_valid_ptr(ptr);
+    ck_assert_msg(ptr == NULL, "Expected NULL pointer to be returned.");
     smalloc_allocator = (s_allocator) {malloc, free};
 } END_TEST
 #endif
 
 TFun misc_tests[] = {
     test_more_args,
+    test_alloc_failure,
+    test_zero_size,
     NULL
 };
